4c6d494aaebb8bf27e5f42e69d4d7eab0326c9a9
ever077 <everman77@gmail.com>
2023-05-22 21:27:12 +0300

    Implementacion de capa Logica: Creacion de Lpersonal. Implementacion de capa Datos: Creacion de ConexionMaestra, Dpersonal e implementacion de funciones Insertar/Editar/Eliminar personal.

---
+++ Datos/ConexionMaestra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Orus.Datos
+{
+    internal static class ConexionMaestra
+    {
+        public static string conexion = "Data source=LAPTOP-PMUGS5GJ\\SQLEXPRESS; Initial catalog=ORUS; Integrated Security=true";
+        public static SqlConnection conectar = new SqlConnection(conexion);
+
+        public static void abrir()
+        {
+            if (conectar.State == ConnectionState.Closed)
+            {
+                conectar.Open();
+            }
+        }
+
+        public static void cerrar()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
---
+++ Datos/Dpersonal.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Orus.Logica;
+using System.Windows.Forms;
+
+namespace Orus.Datos
+{
+    public class Dpersonal
+    {
+        SqlCommand cmd = new SqlCommand();
+        public bool InsertarPersonal(Lpersonal parametros)
+        {
+            try
+			{
+				ConexionMaestra.abrir();
+				cmd = new SqlCommand("insertar_personal", ConexionMaestra.conectar);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+				cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+				cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+				cmd.Parameters.AddWithValue("@Id_cargo", parametros.Id_cargo);
+				cmd.Parameters.AddWithValue("@SueldoPorHora", parametros.SueldoPorHora);
+				if (cmd.ExecuteNonQuery() != 0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				// Muestra el mensaje que figura en el procedimiento SQL
+				MessageBox.Show(ex.Message);
+				return false;
+			}
+			finally
+			{
+				ConexionMaestra.cerrar();
+			}
+        }
+
+        public bool EditarPersonal(Lpersonal parametros)
+        {
+            try
+            {
+                ConexionMaestra.abrir();
+                cmd = new SqlCommand("editar_personal", ConexionMaestra.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id_personal", parametros.Id_personal);
+                cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+                cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+                cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+                cmd.Parameters.AddWithValue("@Id_cargo", parametros.Id_cargo);
+                cmd.Parameters.AddWithValue("@SueldoPorHora", parametros.SueldoPorHora);
+                if (cmd.ExecuteNonQuery() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Muestra el mensaje que figura en el procedimiento SQL
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                ConexionMaestra.cerrar();
+            }
+        }
+
+        public bool EliminarPersonal(Lpersonal parametros)
+        {
+            try
+            {
+                ConexionMaestra.abrir();
+                cmd = new SqlCommand("eliminar_personal", ConexionMaestra.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id_personal", parametros.Id_personal);
+                if (cmd.ExecuteNonQuery() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Muestra el mensaje que figura en el procedimiento SQL
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                ConexionMaestra.cerrar();
+            }
+        }
+    }
+}
