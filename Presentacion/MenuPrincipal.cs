437be6ebd252156f9eba5cf1a28e2fc7b751144c
ever077 <everman77@gmail.com>
2023-11-10 00:33:14 -0300

    Creacion del Usuario Principal. Creacion automatica de los Modulos. Verificacion de los permisos al desplegar el Menu Principal.

--- Presentacion/MenuPrincipal.cs
+++ Presentacion/MenuPrincipal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Orus.Datos;
+using Orus.Logica;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,24 @@ namespace Orus.Presentacion
 {
     public partial class MenuPrincipal : Form
     {
+        private int idUsuario;
+        private string loginV;
+        private Image iconoUsuario;
+
+        public void set_idUsuario(int id)
+        {
+            idUsuario = id;
+        }
+        public void set_loginV(string log)
+        {
+            loginV = log;
+        }
+
+        public void set_iconoUsuario(Image icono)
+        {
+            iconoUsuario = icono;
+        }
+
         public MenuPrincipal()
         {
             InitializeComponent();
@@ -20,6 +40,60 @@ namespace Orus.Presentacion
         private void MenuPrincipal_Load(object sender, EventArgs e)
         {
             panel_Bienvenida.Dock = DockStyle.Fill;
+            validarPermisos();
+        }
+
+        private void validarPermisos()
+        {
+            Lpermiso parametros = new Lpermiso();
+            Dpermiso funcion = new Dpermiso();
+            DataTable dtPermisos = new DataTable();
+
+            parametros.Id_Usuario = idUsuario;
+            funcion.MostrarPermiso(ref dtPermisos, parametros);
+
+            // Inhabilito todos los botones del Menu Principal.
+            btn_Consultas.Enabled = false;
+            btn_Personal.Enabled = false;
+            btn_Registro.Enabled = false;
+            btn_Usuarios.Enabled = false;
+            btn_RestaurarBd.Enabled = false;
+            btn_Respaldos.Enabled = false;
+            btn_Estaciones.Enabled = false;
+
+            foreach (DataRow rowPermiso in dtPermisos.Rows)
+            {
+                string modulo = Convert.ToString(rowPermiso["Modulo"]);
+
+                if (modulo == "PrePlanillas")
+                {
+                    btn_Consultas.Enabled = true;
+                }
+                if (modulo == "Personal")
+                {
+                    btn_Personal.Enabled = true;
+                }
+                if (modulo == "Registro")
+                {
+                    btn_Registro.Enabled = true;
+                }
+                if (modulo == "Usuarios")
+                {
+                    btn_Usuarios.Enabled = true;
+                }
+                if (modulo == "RestauracionBD")
+                {
+                    btn_RestaurarBd.Enabled = true;
+                }
+                if (modulo == "Respaldos")
+                {
+                    btn_Respaldos.Enabled = true;
+                }
+                if (modulo == "Estaciones")
+                {
+                    btn_Estaciones.Enabled = true;
+                }
+            }
         }
 
         private void btn_Personal_Click(object sender, EventArgs e)
